c9c519c6646e4d4e4efed3a6cbc9c06f7848c09e
Ivan Rjevskey <yamschikov.vania@yandex.ru>
2024-06-25 17:02:19 +0300

    update 25.06.24

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+//burger-menu-start//
+document
+    .querySelector(".burger-menu-line")
+    .addEventListener("click", function () {
+        this.classList.toggle("active");
+        document.querySelector(".navigation-menu").classList.toggle("open");
+    });
+//burger-menu-end//
+
+//Modal start//
 document
     .getElementById("open-modal-btn")
     .addEventListener("click", function () {
@@ -25,3 +35,35 @@ document.getElementById("my-modal").addEventListener("click", (event) => {
     if (event._isClickWithInModal) return;
     event.currentTarget.classList.remove("open");
 });
+//Modal end//
+
+//Slider start//
+const images = document.querySelectorAll(".slider-img");
+const controlls = document.querySelectorAll(".controlls");
+let imageIndex = 0;
+
+function show(index) {
+    images[imageIndex].classList.remove("active");
+    images[index].classList.add("active");
+    imageIndex = index;
+}
+
+controlls.forEach((e) => {
+    e.addEventListener("click", (event) => {
+        if (event.target.classList.contains("prev")) {
+            let index = imageIndex - 1;
+            if (index < 0) {
+                index = images.lenght - 1;
+            }
+            show(index);
+        } else if (event.target.classList.contains("next")) {
+            let index = imageIndex + 1;
+            if (index >= images.length) {
+                index = 0;
+            }
+            show(index);
+        }
+    });
+});
+show(imageIndex);
+//Slider end//
